b1bf6a5fb3b4952c2559dfac1f62a1a0ded4284c
John Doe <abdo09adham@gmail.com>
2022-12-27 14:55:09 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:youtube_clone_app/src/app.dart';
+import 'package:youtube_clone_app/src/binding/init_binding.dart';
+import 'package:youtube_clone_app/src/components/youtube_detail.dart';
+import 'package:youtube_clone_app/src/controller/youtube_detail_controller.dart';
+import 'package:youtube_clone_app/src/controller/youtube_search_controller.dart';
+import 'package:youtube_clone_app/src/pages/youtube_search.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Youtube Clone App",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialBinding: InitBinding(),
+      getPages: [
+        GetPage(name: "/", page: () => App()),
+        GetPage(
+          name: "/detail/:videoId",
+          page: () => YoutubeDetail(),
+          binding: BindingsBuilder(
+                () => Get.lazyPut<YoutubeDetailController>(
+                    () => YoutubeDetailController()),
+          ),
+        ),
+        GetPage(
+          name: "/search",
+          page: () => YoutubeSearch(),
+          binding: BindingsBuilder(
+                () => Get.lazyPut<YoutubeSearchController>(
+                    () => YoutubeSearchController()),
+          ),
+        )
+      ],
+    );
+  }
+}
